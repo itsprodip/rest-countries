5dccd7ceb63bacf82bea85b2f3988f659da693ce
nothing36 <prodipsarker953@gmail.com>
2025-11-15 22:26:48 +0600

    counting countries

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -1,22 +1,24 @@
-import React, { use } from 'react';
-import Country from './Country';
-const Countries = ({countryPromise}) => {
-    const countries=use(countryPromise);
-   
-    return (
-      <div>
-        <h1 className="text-4xl font-bold text-green-500 text-center py-10">
-          Here rest countries
-        </h1>
-        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-5'>
-           {
+import React, { use } from "react";
+import Country from "./Country";
+const Countries = ({ countryPromise }) => {
+  const countries = use(countryPromise);
 
-            countries.map(country=><Country key={country.name.common} country={country}></Country>)
-        }
-        </div>
-     
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-green-500 text-center py-10">
+        Here rest countries
+      </h1>
+      <h1 className="text-2xl font-bold text-green-500 text-center">
+       Travelling countries: 
+      </h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-5">
+        {countries.map((country) => (
+          <Country key={country.name.common} country={country}></Country>
+        ))}
       </div>
-    );
+    </div>
+  );
 };
 
 export default Countries;
